970d01f14df774cf32507ae0360106041e0ead0c
Nico Auerswald <auersw.nico@gmail.com>
2022-04-30 12:27:12 +0200

    using Scriptable Asset

--- Assets/Scripts/Channels/GameObjectEventChannel.cs
+++ Assets/Scripts/Channels/GameObjectEventChannel.cs
@@ -1,11 +1,12 @@
 using System;
+using BrieYourself;
 using UnityEngine;
 
 namespace Channels {
     [CreateAssetMenu(fileName = "CH_New_GameObjectEventChannel", menuName = "Channels/GameObjectEventChannel", order = 0)]
-    public class GameObjectEventChannel : ScriptableObject {
+    public class GameObjectEventChannel : ScriptableAsset {
         public Action<GameObject> eventRaised;
-
+        
         public void Raise(GameObject gameObject) {
             eventRaised?.Invoke(gameObject);
         }
