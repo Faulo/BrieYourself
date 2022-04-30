cc89b4933e9a681990ac3cf540de2b2e029c348f
Nico Auerswald <auersw.nico@gmail.com>
2022-04-30 20:16:00 +0200

    added AI Hostile detection

---
+++ Assets/Scripts/Characters/ArtificialInput.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace BrieYourself
+{
+    public class ArtificialInput : MonoBehaviour {
+        [SerializeField] float _hostileDetectionRadius;
+        [SerializeField] LayerMask _whatIsHostile;
+        [SerializeField] Animator _attachedAnimator;
+        Transform _closestHostile;
+        
+        protected void FixedUpdate()
+        { 
+            DetectHostiles();
+        }
+
+        void DetectHostiles()
+        {
+            var hits = Physics.SphereCastAll(this.transform.position, _hostileDetectionRadius,
+                transform.forward, _hostileDetectionRadius, _whatIsHostile);
+            
+            foreach(var hit in hits) {
+                if (!_closestHostile) {
+                    _closestHostile = hit.transform;
+                }
+                float hitDistance = Mathf.Abs(Vector3.Distance(transform.position, hit.transform.position));
+                float closestHostileDistance = Mathf.Abs(Vector3.Distance(transform.position, _closestHostile.position));
+                if (hitDistance < closestHostileDistance) {
+                    _closestHostile = hit.transform;
+                }
+                closestHostileDistance = Mathf.Abs(Vector3.Distance(transform.position, _closestHostile.position));
+                Debug.Log(closestHostileDistance);
+                _attachedAnimator.SetFloat("closestHostileDistance", closestHostileDistance);
+            }
+        }
+        protected void OnValidate() 
+        {
+            if (!_attachedAnimator) 
+            {
+                TryGetComponent(out _attachedAnimator);
+            }
+        }
+    }
+}
