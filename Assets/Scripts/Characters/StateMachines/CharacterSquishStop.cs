cfdb2ac49829b7a5302cff9e208ff202c9c37f46
Daniel Schulz <faulolio@gmail.com>
2022-05-02 13:45:38 +0200

    Tweak character

--- Assets/Scripts/Characters/StateMachines/CharacterSquishStop.cs
+++ Assets/Scripts/Characters/StateMachines/CharacterSquishStop.cs
@@ -1,3 +1,4 @@
+using Slothsoft.UnityExtensions;
 using UnityEngine;
 
 namespace BrieYourself.Characters.StateMachines {
@@ -7,7 +8,7 @@ namespace BrieYourself.Characters.StateMachines {
 
         protected override void StateEnter(in AnimatorStateInfo stateInfo, int layerIndex) {
             if (character.TryGetComponent<Rigidbody>(out var rigidbody)) {
-                rigidbody.velocity = Vector3.up * jumpSpeed;
+                rigidbody.velocity = rigidbody.velocity.WithY(jumpSpeed);
             }
         }
         protected override void StateUpdate(in AnimatorStateInfo stateInfo, int layerIndex) {
@@ -15,6 +16,7 @@ namespace BrieYourself.Characters.StateMachines {
         }
         protected override void StateExit(in AnimatorStateInfo stateInfo, int layerIndex) {
             if (character.TryGetComponent<Rigidbody>(out var rigidbody)) {
+                character.velocity = rigidbody.velocity;
                 Destroy(rigidbody);
             }
             character.boxSize = config.defaultBoxSize;
